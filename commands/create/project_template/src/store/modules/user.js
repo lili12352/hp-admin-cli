5b366e7c89c96b3e42aae731d208603f7c611bf3
lihaipeng <lihaipeng@shanlitech.com>
2024-03-08 10:41:35 +0800

    refactor: 调整格式

--- commands/create/project_template/src/store/modules/user.js
+++ commands/create/project_template/src/store/modules/user.js
@@ -2,20 +2,20 @@ import { getJavaScriptFileSuffix } from "../../../../project_utils/index.js";
 export const storeIndex = (params) => {
   const { answers } = params;
   return `import { defineStore } from "pinia";
-  import { store } from "../index";
-  
-  export const useUserInfoStore = defineStore({
-    id: "user",
-    state: () => ({
-      name: "hhh",
-    }),
-    actions: {},
-  });
-  
-  export const useUserInfoStoreWithOut = () => {
-    return useUserInfoStore(store);
-  };
-  `;
+import { store } from "../index";
+
+export const useUserInfoStore = defineStore({
+  id: "user",
+  state: () => ({
+    name: "hhh",
+  }),
+  actions: {},
+});
+
+export const useUserInfoStoreWithOut = () => {
+  return useUserInfoStore(store);
+};
+`;
 };
 const createFileName = (answers) => {
   const { variant } = answers;
