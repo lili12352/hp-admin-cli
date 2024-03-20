815643f7664cf48483df449fd23dd5869ef6e53c
lihaipeng <lihaipeng@shanlitech.com>
2024-03-20 11:53:11 +0800

    fix: 修改

--- src/commands/create/project_template/src/layouts/components/aside/menu.ts
+++ src/commands/create/project_template/src/layouts/components/aside/menu.ts
@@ -2,7 +2,6 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useSystemStore } from "@/store/modules/system";
 const systemStore = useSystemStore();
-//#slot:ui_1
 const router = useRouter();
 const props = defineProps({
   isCollapse: {
@@ -29,15 +28,6 @@ const defaultActive = ref(router.currentRoute.value.fullPath);
 };
 return {
   slot: {
-    ui_1: {
-      element: function ui_1() {
-        return ``;
-      },
-      antdv: function ui_1() {
-        return `import { useSystemStore } from "@/store/modules/system";
-        const systemStore = useSystemStore();`;
-      },
-    },
     ui_2: {
       element: function ui_1() {
         return `const select = (v: string) => {
