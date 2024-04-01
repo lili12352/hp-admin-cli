1c8443d75870a2fb88f649cb048fc702e841bc10
lihaipeng <lihaipeng@shanlitech.com>
2024-04-01 09:41:38 +0800

    feat: 添加模板

--- src/commands/create/project_template/src/views/option_1/a.vue
+++ src/commands/create/project_template/src/views/option_1/a.vue
@@ -1,3 +1,30 @@
 <template>
-  <div>a</div>
+  <div>a //#model_1 //#hook_1</div>
 </template>
+//#end;
+<script>
+return {
+  slot: {
+    hook_1: {
+      element: function () {
+        return ``;
+      },
+      antdv: function () {
+        return ``;
+      },
+    },
+  },
+  model: {
+    model_1: {
+      i18n: {
+        HOOK: function () {
+          return ``;
+        },
+        FALSE: function () {
+          return "";
+        },
+      },
+    },
+  },
+};
+</script>
